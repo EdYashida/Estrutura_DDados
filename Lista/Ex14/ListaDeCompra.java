18f6affb1019107c0608be193b26199d0d9ba1cc
EdYashida <107697323+EdYashida@users.noreply.github.com>
2022-07-22 22:30:05 -0300

    Create ListaDeCompra.java

---
+++ Lista/Ex14/ListaDeCompra.java
@@ -0,0 +1,100 @@
+public class ListaDeCompra extends ListaEstatica {
+    
+    Produto lista[] = new Produto[3];
+    int ultimo = -1;
+    
+    @Override
+    public void insere(Produto p){
+        
+        if(ultimo < 12){
+            
+            ultimo++;
+            lista[ultimo] = p;
+            System.out.println("Elemento inserido ");
+            
+        }else{
+            
+            System.out.println("Erro");
+            
+        }
+        
+    }
+    
+    @Override
+    public void limpaLista(){
+        
+        for(int i =0; i < ultimo; i++){
+            
+            lista[i] = null;
+            
+        }
+        
+    }
+    
+    @Override
+    public String busca(String n){
+        
+        for(int i =0; i < ultimo; i++){
+            
+            if(lista[i].nomeProduto.contains(n)){
+                
+                return lista[i].toString();
+                
+            }
+            
+        }
+        return null;
+        
+    }
+    
+    @Override
+    public void remove(String n){
+        
+        for(int i =0; i < ultimo; i++){
+            
+            if(lista[i].nomeProduto.contains(n)){
+                
+               lista[i] = null;
+                
+            }
+            lista[i] = lista[i+1];
+            
+        }
+        
+        
+    }
+    
+    @Override
+    public void imprimeLista(){
+        
+        for(int i = 0; i < 2; i++){
+            
+            if(lista[i] == null){
+                
+                System.out.println("Lista vazia ");
+                break;
+            }
+            System.out.println("Elemento "+i+" : "+lista[i].toString());
+            
+        }
+        
+    }
+    
+    public void calculoTotal(){
+        
+        double somaTotal=0;
+        
+        for(int i = 0; i < 2; i++){
+            
+            double totalPProduto=0;
+            totalPProduto = totalPProduto + (lista[i].quantidade * lista[i].valor);
+            System.out.println(" "+lista[i].valor +" "+" "+ lista[i].quantidade+" "+" "+totalPProduto);
+            somaTotal = somaTotal + totalPProduto;
+            
+        }
+        
+        System.out.println("Valor total da compra: "+ somaTotal);
+        
+    }
+    
+}
