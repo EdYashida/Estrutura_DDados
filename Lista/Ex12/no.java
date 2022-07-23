dccba8e666a79146e05a6511b457664433f8928e
EdYashida <107697323+EdYashida@users.noreply.github.com>
2022-07-22 21:31:23 -0300

    Create no.java

---
+++ Lista/Ex12/no.java
@@ -0,0 +1,13 @@
+public class No {
+    
+    int num;
+
+    public No(String n, int i){
+        this.num = i;
+    }
+
+    public String tostring(){
+        return  " numero: "+num;
+    }
+    
+}
