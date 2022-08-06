59df9519d34e314b73c3e573c575262185e97769
EdYashida <107697323+EdYashida@users.noreply.github.com>
2022-08-05 23:36:33 -0300

    Update atleta.java

--- Lista Dinâmica/Atividade 2/atleta.java
+++ Lista Dinâmica/Atividade 2/atleta.java
@@ -1,10 +1,10 @@
 public class Atleta {
     
-    String nome;
-    float altura;
-    float peso;
-    String esporte;
-    String patrocinadores;
+    private String nome;
+    private float altura;
+    private float peso;
+    private String esporte;
+    private String patrocinadores;
     
    
     public Atleta(String n, float a, float pe, String e, String pa){
@@ -14,6 +14,34 @@ public class Atleta {
         this.esporte=e;
         this.patrocinadores=pa;
     }
+
+    Atleta() {
+        this.nome=null;
+        this.altura=0;
+        this.peso=0;
+        this.esporte=null;
+        this.patrocinadores=null;
+    }
+    
+    public void setNome(String nome){
+    this.nome = nome;
+  }
+
+  public void setEsporte(String esporte){
+    this.esporte = esporte;
+  }
+
+  public void setPatrocinadores(String pa){
+    this.patrocinadores = pa;
+  }
+
+  public void setAltura(float a){
+    this.altura = a;
+  }
+
+  public void setPeso(float pe){
+    this.peso = pe;
+  }
     
     public String toString(){
         return "Nome: "+nome+" | Altura: "+altura+" | Peso: "+peso+" | Esporte: "+esporte+" | Patrocinadores: "+patrocinadores;
