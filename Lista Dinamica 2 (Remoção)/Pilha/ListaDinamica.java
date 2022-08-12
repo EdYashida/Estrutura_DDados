2caaaeb78810ce4bfc3536378c5991575852b032
EdYashida <107697323+EdYashida@users.noreply.github.com>
2022-08-12 17:04:46 -0300

    Create ListaDinamica.java

---
+++ Lista Dinamica 2 (Remoção)/Pilha/ListaDinamica.java
@@ -0,0 +1,50 @@
+public class ListaDinamica{
+  No inicio;
+  No fim;
+
+  public void insere(No n){
+    if (inicio == null){
+        inicio = n;
+        fim = n;
+    }
+    else{
+      fim.proximo = n;
+      fim = n;
+    }
+  }
+
+public void imprime(){
+  No aux = inicio;
+  while (aux!=null){
+    System.out.println(aux.toString());
+    aux = aux.proximo;
+  }
+}
+
+public void remove(){   //remocao no fim
+
+    if(inicio == null){
+        System.out.println("Não foi possivel remover nada, pois a lista esta vazia");
+    }
+    
+    else{
+    No aux = inicio;
+    No remover = null;
+    No anterior = null;
+    while(aux!=null){
+      if (aux==fim){
+        remover = aux;
+        break;
+      }
+      anterior = aux;
+      aux = aux.proximo;
+      
+      
+    }
+
+        fim = anterior;
+        anterior.proximo=null;
+
+    }
+}
+}
