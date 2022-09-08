445d832aff2e2f609974bbcce35634da454f614f
EdYashida <107697323+EdYashida@users.noreply.github.com>
2022-09-08 20:32:21 -0300

    Update no.java

--- Lista 2/Ex 3/no.java
+++ Lista 2/Ex 3/no.java
@@ -1,1 +1,18 @@
-asd
+public class no {
+    no proximo;
+    pessoa p;
+    
+    public no(pessoa p){
+        this.p=p;
+    }
+    
+    
+    public String toString(){
+        if(proximo==null){
+        return this.hashCode()+"  Pessoa "+p.toString();
+        }
+        else{
+        return this.hashCode()+ "  Pessoa "+p.toString()+"|| proximo:"+proximo.hashCode();    
+        }
+    }
+}
