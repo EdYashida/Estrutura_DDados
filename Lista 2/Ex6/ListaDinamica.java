8fe71a5f002809173ba4dea4078e1a046002fb18
EdYashida <107697323+EdYashida@users.noreply.github.com>
2022-09-09 12:08:28 -0300

    Create ListaDinamica.java

---
+++ Lista 2/Ex6/ListaDinamica.java
@@ -0,0 +1,36 @@
+public class ListaDinamica {
+   No inicio;
+   No fim;
+   
+   public void cadastraAluno(No a){
+       if(inicio==null){
+           inicio=a;
+           fim=a;
+       }
+       else{
+           fim.setProximo(a);
+           a.setAnterior(fim);
+           fim=a;    
+       }
+   }
+   
+   public void imprimeAluno(String nome){
+       No aux=inicio;
+       
+       while(aux!=null){
+           if(aux.toString().contains(nome)){
+               System.out.println(aux.toString());
+           }
+           aux=aux.getProximo();
+       }
+   }
+   
+   public void mostraTodos(){
+       No aux=inicio;
+       
+       while(aux!=null){
+           System.out.println(aux.toString());
+           aux=aux.getProximo();
+       }
+   }
+}
