b87bab190648d70daa40e79059e7a6e61c2c5769
EdYashida <107697323+EdYashida@users.noreply.github.com>
2022-10-28 18:56:21 -0300

    Update Main.java

--- Recursividade/Rec1/Main.java
+++ Recursividade/Rec1/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Recursividade1 {
     
     public int soman(int n){
@@ -10,9 +11,15 @@ public class Recursividade1 {
     }
 
     public static void main(String[] args) {
+      Scanner teclado = new Scanner(System.in);
+        
       Recursividade1 r = new Recursividade1();
       
-     int i= r.soman(7);
+      System.out.println("Digite o valor dos n primeiros numeros inteiros");
+        
+      int j = teclado.nextInt();
+      
+     int i= r.soman(j);
      
       System.out.println("A soma dos n primeiros numero inteiro eh de: "+i);
     }  
